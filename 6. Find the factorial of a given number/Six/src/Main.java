9b57190c5f58b61c45c4e5a3e6660d136ccfbc3a
Pha raoh <pharaoh1822005@gmail.com>
2026-02-06 09:19:27 +0600

    For Lab: Task six complete

---
+++ 6. Find the factorial of a given number/Six/src/Main.java
@@ -0,0 +1,14 @@
+
+//6. Find the factorial of a given number.
+
+public class Main {
+    public static void main(String[] args){
+        int num = 5;
+        int fact = 1;
+
+        for(int i = 1; i <= num; i++){
+            fact *= i;
+        }
+        System.out.println("Factorial = " +fact);
+    }
+}
